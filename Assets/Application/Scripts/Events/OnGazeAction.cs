02b2c21973172187853f4dc4bb75d7cbaf59dbe6
Krystian Babilinski <kbabilinski@gmail.com>
2016-10-08 14:13:20 -0400

    On Gaze Events

---
+++ Assets/Application/Scripts/Events/OnGazeAction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class OnGazeAction : MonoBehaviour , IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler {
+
+	public UnityEvent OnClick ();
+	public UnityEvent OnEnter ();
+	public UnityEvent OnExit ();
+
+
+	public void OnPointerClick(PointerEventData data){
+
+		OnClick.Invoke();
+	}
+
+	public void OnPointerEnter(PointerEventData data){
+
+		OnEnter.Invoke();
+
+	}
+
+	public void OnPointerExit(PointerEventData data){
+
+		OnExit.Invoke();
+	}
+
+}
