2202522837e08acd6bba8e3c7ec44647c7e2cb99
Zachery Schiller <zacheryschiller@gmail.com>
2016-10-09 12:35:26 -0400

    Added audio to ice cream

---
+++ Assets/Application/Scripts/IceCreamScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySpeechToText.Widgets;
+
+public class IceCreamScript : MonoBehaviour {
+
+	bool iceCreamQuestStart = false;
+	public GameObject audioGameObject;
+	public AudioSource iceCreamAudio;
+
+	public void wantIceCream() {
+		if (!iceCreamQuestStart) {
+			StartCoroutine (wantIceCreamUpdate ());
+			iceCreamQuestStart = true;
+		}
+	}
+
+	IEnumerator wantIceCreamUpdate (){
+		//iceCreamAudio.clip = audioGameObject.transform.GetComponent<IceCreamManAI> ().Quest [15];
+		iceCreamAudio.Play ();
+		yield return new WaitUntil(() => iceCreamAudio.isPlaying == false);
+	}
+}
