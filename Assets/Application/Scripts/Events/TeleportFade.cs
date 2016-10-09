5bdd45845949ab9fb7681fb7d6fac4aa66ac5094
Jeffrey King <king@Jeffreys-MacBook-Pro-4.local>
2016-10-09 09:06:23 -0400

    minor changes to teleport script

--- Assets/Application/Scripts/Events/TeleportFade.cs
+++ Assets/Application/Scripts/Events/TeleportFade.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TeleportFade : MonoBehaviour {
 
+	public Canvas fadeCanvas; 
+
+	void Start() {
+		fadeOnTeleport ();
+	}
+
 	public void teleportToTransform(Transform playerPosition, Transform newPosition) {
 		playerPosition.position = newPosition.position;
+	}
+
+	public void fadeOnTeleport() {
+		
+	}
+
+	IEnumerator FadeToBlack() {
+		
 	}
 }
