468b94f132760bcd8e34187fdd3bb6f90ee47f9f
Jeffrey King <king@Jeffreys-MacBook-Pro-4.local>
2016-10-09 10:28:56 -0400

    add fade

--- Assets/Application/Scripts/Events/TeleportFade.cs
+++ Assets/Application/Scripts/Events/TeleportFade.cs
@@ -6,10 +6,6 @@ public class TeleportFade : MonoBehaviour {
 
 	public Canvas fadeCanvas; 
 
-	void Start() {
-		fadeOnTeleport ();
-	}
-
 	public void teleportToTransform(Transform playerPosition, Transform newPosition) {
 		Vector3 TempPos = newPosition.position;
 		TempPos.y = playerPosition.position.y;
@@ -17,10 +13,22 @@ public class TeleportFade : MonoBehaviour {
 	}
 
 	public void fadeOnTeleport() {
-		
+		StartCoroutine (FadeToBlack(1.0f, 1.0f));
 	}
-	/*
-	IEnumerator FadeToBlack() {
-		
-	}*/
+
+	IEnumerator FadeToBlack(float aValue, float aTime) {
+		Color canvasImageColor = fadeCanvas.GetComponent<Image>().color;
+		Debug.Log (canvasImageColor);
+		float alpha = fadeCanvas.GetComponent<Image> ().color.a;
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+		{
+			Color newColor = new Color(0, 0, 0, Mathf.Lerp(alpha,aValue,t));
+			fadeCanvas.GetComponent<Image>().color = newColor;
+			yield return null;
+		}
+		yield return new WaitForSeconds (aTime);
+		Color newCanvasColor = fadeCanvas.GetComponent<Image>().color;
+		newCanvasColor.a = 0.0f;
+		fadeCanvas.GetComponent<Image>().color = newCanvasColor;
+	}
 }
